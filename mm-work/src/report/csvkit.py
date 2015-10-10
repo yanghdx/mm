dbb5d26dcafe772c68f8b11affbc51739e2a1ffb
yanghdx <yanghdx@163.com>
2015-10-10 13:16:20 +0800

    读写csv file方法

--- mm-work/src/report/csvkit.py
+++ mm-work/src/report/csvkit.py
@@ -1,12 +1,32 @@
-import csv
+import csv, os
+'''
+读取csv file
+'''
+def readCsv(path, mode='r'):
+    result = [];
+    if os.path.isfile(path):
+        csvfile = open(path, mode, newline='')
+        reader = csv.reader(csvfile)
+        count = 0;
+        for row in reader:
+            if count > 0 :
+                result.append(row);
+            count += 1
+            #print (','.join(row))
+        
+    return result
 
-csvfile = open('e:/test.csv', 'w', newline='')
-writer = csv.writer(csvfile)
-writer.writerow(['姓名', '年龄', '电话'])
+'''
+写入csv file
+'''
+def writeCsv(path, mode='w', title=[], data=[]):
+    csvfile = open(path, 'w', newline='')
+    writer = csv.writer(csvfile)
+    writer.writerow(title)
 
-data = [('jack2', 28, '151511111'),
-        ('张芳2', 11, '112222222')]
-writer.writerows(data)
-csvfile.close()
+    writer.writerows(data)
+    csvfile.close()
 
-print('执行成功!')
+
+
+    
